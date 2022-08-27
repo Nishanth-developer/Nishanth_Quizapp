09522e5dd98d1d5aff3b6dcccbc73abe2553bc77
Nishanth-developer <augustcoder24x7@gmail.com>
2022-08-27 22:59:14 +0530

    initial commit

---
+++ js/question.js
@@ -0,0 +1,35 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Who is the founder of HTML",
+    answer: "Tim Berners-Lee",
+    options: [
+      "Charles Darwin",
+      "Tim Berners-Lee",
+      "Bill Gates",
+      "Elon Musk"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Who is the founder of Javascript",
+    answer: "Brendan Eich",
+    options: [
+      "Bill Gates",
+      "Tesla",
+      "Brendan Eich",
+      "Nishanth"
+    ]
+  },
+];
